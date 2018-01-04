797c10c2b8e52b40284909e62e918aff3654c55d
Jordan Kline <jordan.kline@gmail.com>
2018-01-04 23:18:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,11 @@
 katz_deli = []
 
-def line
-
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  p "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
 end
 
-def take_a_number
+def line(katz_deli)
 
 end
 
