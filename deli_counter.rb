c2867b1be1a65db3b3ed125a7fa5208beafbdd64
Jordan Kline <jordan.kline@gmail.com>
2018-01-04 23:24:23 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,8 +7,12 @@ end
 
 def line(katz_deli)
   return_phrase = "The line is currently: "
-  katz_deli.each_with_index { |el, i| return_phrase << "#{i + 1}. " << el }
-  puts return_phrase
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    katz_deli.each_with_index { |el, i| return_phrase << "#{i + 1}. " << el }
+    puts return_phrase
+  end
 end
 
 def now_serving(katz_deli)
