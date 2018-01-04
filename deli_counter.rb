76473c151f336de69a0bf9d4d2ababf71c8cf250
Jordan Kline <jordan.kline@gmail.com>
2018-01-04 23:22:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,15 +2,15 @@ katz_deli = []
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  p "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
 end
 
 def line(katz_deli)
   return_phrase = "The line is currently: "
   katz_deli.each_with_index { |el, i| return_phrase << "#{i + 1}. " << el }
-  p return_phrase
+  puts return_phrase
 end
 
 def now_serving(katz_deli)
-  p "Currently serving #{katz_deli[0]}."
+  puts "Currently serving #{katz_deli[0]}."
 end
